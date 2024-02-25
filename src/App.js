318a651f326a0d6a7bad00e6675f55e3fceaa995
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-02-25 20:45:21 +0300

    Food

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import HeadLineCards from "./components/HeadLineCards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Food from "./components/Food";
+import Category from "./components/Category";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Hero />
       <HeadLineCards />
+      <Food />
+      <Category />
     </div>
   );
 }
